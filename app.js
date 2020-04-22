b716227154a6578d31be949b5ee9bced46c6394d
Carlos Enrique <enrique_lcbjj@hotmail.com>
2020-04-21 21:33:49 -0500

    Sending responses (Unusual way)

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     console.log(req) // Important: req.url, req.method, req.headers
     // process.exit();
+
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My first page</title></head>");
+    res.write("<body><h1>Hello from my server</h1></body>");
+    res.write("</html>");
+    res.end();
 });
 
 server.listen(3000);
