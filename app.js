41b683d950c54357d38667cc40e3fa74aedda293
Carlos Enrique <enrique_lcbjj@hotmail.com>
2020-04-21 21:22:26 -0500

    Entendiendo requests

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require("http"); 
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    console.log(req) // Important: req.url, req.method, req.headers
+    // process.exit();
 });
 
 server.listen(3000);
