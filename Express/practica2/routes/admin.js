9ef202c03d247a4560cdeb6c66fcd34bf91cb85a
Carlos Enrique <enrique_lcbjj@hotmail.com>
2020-04-24 01:26:07 -0500

    Creando simplificador del path

--- Express/practica2/routes/admin.js
+++ Express/practica2/routes/admin.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const express = require("express");
+const rootDir = require("../util/path")
 
 const router = express.Router();
 
 
 router.get("/add-product", (seq, res, next) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+    res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
