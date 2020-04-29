aa297a97f4b176ef73c8e24538410ae348391594
Carlos Enrique <enrique_lc@ciencias.unam.mx>
2020-04-29 02:05:18 -0500

    Arreglando 404 not found y creando MVC folder

--- 3. Template engine/Template engine with ejs/app.js
+++ 3. Template engine/Template engine with ejs/app.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
 });
 
-app.listen(3001);
+app.listen(3000);
